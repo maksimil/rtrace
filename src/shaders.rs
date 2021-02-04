f52d37adb201b710023dfc81fc407b71fb76291a
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-02-04 22:08:22 +0300

    Renamed all the shaders to glsl

--- src/shaders.rs
+++ src/shaders.rs
@@ -2,22 +2,22 @@ macro_rules! declare_shaders {
     ($name: ident) => {
         pub mod $name {
             pub const VERTEX: &str =
-                include_str!(concat!("shaders/", stringify!($name), "/vertex.ocl"));
+                include_str!(concat!("shaders/", stringify!($name), "/vertex.glsl"));
             pub const FRAGMENT: &str =
-                include_str!(concat!("shaders/", stringify!($name), "/fragment.ocl"));
+                include_str!(concat!("shaders/", stringify!($name), "/fragment.glsl"));
             pub const GEOMETRY: Option<&str> = None;
         }
     };
     ($name: ident +geometry) => {
         pub mod $name {
             pub const VERTEX: &str =
-                include_str!(concat!("shaders/", stringify!($name), "/vertex.ocl"));
+                include_str!(concat!("shaders/", stringify!($name), "/vertex.glsl"));
             pub const FRAGMENT: &str =
-                include_str!(concat!("shaders/", stringify!($name), "/fragment.ocl"));
+                include_str!(concat!("shaders/", stringify!($name), "/fragment.glsl"));
             pub const GEOMETRY: Option<&str> = Some(include_str!(concat!(
                 "shaders/",
                 stringify!($name),
-                "/geometry.ocl"
+                "/geometry.glsl"
             )));
         }
     };
